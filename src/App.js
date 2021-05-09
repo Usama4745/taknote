f733d68ba25f895a1fe982483ae18610ae796857
Usama Ahmed <usama4745@gmail.com>
2021-05-09 20:08:22 +0500

    pushing after adding save button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 
 import NotesList from './components/NotesList';
-import {useState} from 'react';
-import {nanoid} from 'nanoid';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 const App = () => {
-  const[notes,setNotes]=useState([
+  const [notes, setNotes] = useState([
     {
-    id:nanoid(),
-    text:'this is my first note',
-    date:"15/04/2021"
-  },
+      id: nanoid(),
+      text: 'this is my first note',
+      date: "15/04/2021"
+    },
     {
-    id:nanoid(),
-    text:'this is my second note',
-    date:"15/04/2021"
-  },
+      id: nanoid(),
+      text: 'this is my second note',
+      date: "15/04/2021"
+    },
     {
-    id:nanoid(),
-    text:'this is my thirs note',
-    date:"15/04/2021"
+      id: nanoid(),
+      text: 'this is my thirs note',
+      date: "15/04/2021"
+    }
+  ])
+  const addNote = (text) => {
+    const date=new Date();
+    const newNote={
+      id:nanoid(),
+      text:text,
+      date:date.toLocaleDateString(),
+    }
+    const newNotes=[...notes,newNote];
+    setNotes(newNotes);
   }
-])
   return (
-  <div className='container'> 
-    <NotesList notes={notes}/>
-  </div>
+    <div className='container'>
+      <NotesList notes={notes} handleAddNote={addNote} />
+    </div>
   )
 }
 
--- src/components/AddNode.js
+++ src/components/AddNode.js
@@ -4,13 +4,16 @@
 import { useState } from 'react';
 
 
-const AddNote = () => {
+const AddNote = ({handleAddNote}) => {
     const [noteText, setNoteText] = useState('')
 
 
 
     const handleChange = (event) => {
         setNoteText(event.target.value);
+    }
+    const handleSaveClick = () => {
+        handleAddNote(noteText);
     }
     return (
         <div className="note new">
@@ -21,7 +24,7 @@ const AddNote = () => {
             ></textarea>
             <div className="note-footer">
                 <small>200 Remaining</small>
-                <button className="save">Save</button>
+                <button className="save" onClick={handleSaveClick}>Save</button>
             </div>
         </div>
     )
