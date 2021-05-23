f9cfc9a8fed4a0d94226d5fc6fad4aadc247104e
Usama Ahmed <usama4745@gmail.com>
2021-05-23 17:46:35 +0500

    added code for dark mode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NotesList from './components/NotesList';
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import Search from './components/Search';
-
+import Header from './components/Header';
 const App = () => {
   const [notes, setNotes] = useState([
     {
@@ -33,15 +33,22 @@ const App = () => {
     setNotes(newNotes);
   }
   const [searchText, setSearchText] = useState('');
+
+  const [darkMode, setDarkMode] = useState(false);
+
   const deleteNote = (id) => {
     const newNotes = notes.filter((note) => note.id != id);
     setNotes(newNotes);
   }
   return (
-    <div className='container'>
-      <Search handleSearchNote ={setSearchText}/>
-      <NotesList notes={notes.filter((note)=>note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote} />
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className='container'>
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))} handleAddNote={addNote} handleDeleteNote={deleteNote} />
+      </div>
     </div>
+
   )
 }
 
